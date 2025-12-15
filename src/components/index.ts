29b17e4bdd76a58918de4e22aa343bdbd0bf953c
Monica Mateescu <monica.antonie@gmail.com>
2025-12-15 18:23:33 +0100

    display artworks on home page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as NavBar } from "./ui/NavBar";
 export { default as SideBar } from "./ui/SideBar";
 export { default as Hero } from "./ui/Hero";
+export { default as ArtworkCard } from "./cards/ArtworkCard";
+export { default as ArtworksList } from "./map/ArtworksList";
 export { default as Footer } from "./ui/Footer";
