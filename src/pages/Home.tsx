3e5c33cd08b41f9061bf2bdb19344dd870e818b9
Monica Mateescu <monica.antonie@gmail.com>
2025-12-16 08:36:46 +0100

    replace Hero with search form on Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router";
-import { Hero, ArtworksList } from "../components";
+import { useLoaderData, Form } from "react-router";
+
+import { ArtworksList } from "../components";
 import type { Artworks } from "../types";
 
 const Home = () => {
@@ -7,12 +8,29 @@ const Home = () => {
   const artworks = initialData.data;
   const imgUrl = initialData.config.iiif_url;
   return (
-    <>
-      <Hero />
-      <main className="p-15 mx-auto">
+    <main className="p-15 mx-auto">
+      <section className="mb-10">
+        <h1 className="text-xl text-shadow-sm mb-5">Artworks</h1>
+        <p className="mb-5 text-md font-bold">
+          Discover, admire, and save your favorite artworks from the museum’s
+          collection.
+        </p>
+        <Form method="get" className="w-full flex gap-1 justify-between">
+          <input
+            type="text"
+            name="q"
+            placeholder="Search artworks"
+            className="input w-full"
+          />
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </Form>
+      </section>
+      <section>
         <ArtworksList artworks={artworks} imgUrl={imgUrl} />
-      </main>
-    </>
+      </section>
+    </main>
   );
 };
 
