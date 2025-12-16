8cecfcf3201789429d8e4fa3d8b768c383d15662
Monica Mateescu <monica.antonie@gmail.com>
2025-12-16 11:06:49 +0100

    add save to gallery button and update artwork card style

--- src/components/cards/ArtworkCard.tsx
+++ src/components/cards/ArtworkCard.tsx
@@ -9,21 +9,25 @@ const ArtworkCard = ({
 }) => {
   const { title, artist_title, image_id } = artwork;
   return (
-    <div className="card bg-base-200 w-96 shadow-lg">
-      <figure className="bg-base-300 aspect-4/3">
+    <div className="card w-96 shadow-lg">
+      <figure className="aspect-4/3">
         {image_id && (
           <img
             src={`${imgUrl}/${image_id}/full/200,/0/default.jpg`}
             alt={title}
-            className="object-cover h-full w-full"
+            className="object-center h-full w-full"
           />
         )}
       </figure>
 
       <div className="card-body">
-        <h2 className="card-title text-sm">
-          {artist_title ?? "Artist unknown"} : {title}
-        </h2>
+        <h2 className="card-title text-sm">{title}</h2>
+        <h3 className="text-neutral-400">{artist_title ?? "Artist unknown"}</h3>
+        <div className="card-actions justify-end">
+          <button className="btn btn-outline btn-primary">
+            Save to gallery
+          </button>
+        </div>
       </div>
     </div>
   );
